b944d88da94d2401a12605d6efe8af8d830cb076
llocr <36-96@naver.com>
2024-05-14 16:01:02 +0900

    init project

---
+++ src/main/java/com/sparta/schedule/NbcampSpringScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampSpringScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampSpringScheduleApplication.class, args);
+    }
+
+}
